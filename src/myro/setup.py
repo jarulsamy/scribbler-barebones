8a6c4a86eaacec73cd3a1d5b63117f2651abff43
jarulsamy <joshua.gf.arul@gmail.com>
2020-03-12 22:30:30 -0600

    Added setup

---
+++ src/myro/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("myro/README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="myro",
+    version="0.0.1",
+    scripts=["myro/"],
+    author="Joshua Arulsamy",
+    author_email="joshua.gf.arul@gmail.com",
+    description="A robot communication library for Scribbler / Scribbler 2",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jarulsamy/scribbler-barebones",
+    packages=setuptools.find_packages(),
+    classifiers=[
+            "Programming Language :: Python :: 3.7",
+            "Licence :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+    ],
+)
